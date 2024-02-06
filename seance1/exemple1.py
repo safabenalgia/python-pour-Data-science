769223f5401763023e8a5f2fc7f0fed3733af496
safabenalgia <benalgia4@gmail.com>
2024-02-06 13:17:02 +0100

    variables et affichage

---
+++ seance1/exemple1.py
@@ -0,0 +1,10 @@
+# en mode script ,on ecrit les codes dans un fichier .py
+nom="Ali" #variable de type str
+age=30 # variable de type int
+salaire=1200.7654 #variable de type float
+est_celibataire=True  #variable dy type booléen
+#afficher les infos de la personne
+# print pour afficher 
+print(nom)
+#print("hello","word",sep=";",end='|')
+print('l\'employe "', nom,"\" a comme salaire  ",salaire,"et comme age ", age," celibataire:",est_celibataire,sep="")
